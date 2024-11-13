9daf273e442ab681708d863174f86734c07e4a73
DanteWHS <dante130283@student-schoolsofwestfield.org>
2024-11-13 16:17:58 +0000

    done

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 function main(){
-    let uChoice = 0;
-    let cChoice = 0;
-    while (uChoice==cChoice) {
-        uChoice = uTurn();
-        cChoice = cTurn();
-        if (uChoice==cChoice) alert("we both chose " + cChoice);
+    let youWin = 0
+    let iWin = 0
+    for (let x = 1; x<=3; x++){
+        alert("Round "+x);
+        let uChoice = 0;
+        let cChoice = 0;
+        while (uChoice==cChoice) {
+            uChoice = uTurn();
+            cChoice = cTurn();
+            if (uChoice==cChoice) alert("we both chose " + cChoice);
+        }
+        let win = findWinner(uChoice, cChoice);
+        if (win=="You") youWin+=1;
+        else iWin+=1;
     }
-    findWinner(uChoice, cChoice);
+    if (youWin>iWin) alert("You won " + youWin + " of 3 rounds, you win!");
+    else alert("You won " + youWin + " of 3 rounds, computer wins!");
 }
 
 function uTurn() {
@@ -27,8 +36,9 @@ function cTurn(){
 
 function findWinner(uChoice, cChoice){
     let winArray = [["r", "p", "Computer"], ["p", "s", "Computer"], ["s", "r", "Computer"], ["r", "s", "You"], ["s", "p", "You"], ["p", "r", "You"]];
-    for (let i = 0; i<winArray.length; i++){
+    for (var i = 0; i<winArray.length; i++){
         if (winArray[i][0]+winArray[i][1]==uChoice+cChoice) break;
     }
     alert(uChoice + " Vs. " + cChoice + ". " + winArray[i][2] + " won!");
+    return winArray[i][2];
 }
